6218b987b8828bd1c9b8fbc86eded6694e678cd4
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-10-17 21:34:39 +0530

    Completed the frontendmentor project

---
+++ README.md
@@ -0,0 +1,70 @@
+# Frontend Mentor - Time tracking dashboard
+
+![Design preview for the Time tracking dashboard coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+- [Frontend Mentor - Time tracking dashboard](#frontend-mentor---time-tracking-dashboard)
+  - [Welcome! 👋](#welcome-)
+  - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+  - [Desktop](#desktop)
+  - [Mobile](#mobile)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [Useful resources](#useful-resources)
+
+## The challenge
+
+Your challenge is to build out this dashboard and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+If you would like to practice working with JSON data, we provide a local `data.json` file for the activities. This means you'll be able to pull the data from there instead of using the content in the `.html` file.
+
+Your users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
+
+### Screenshot
+
+## Desktop
+
+![](./screenshot-desktop.png)
+
+## Mobile
+
+![](./screenshot-mobile.png)
+
+### Links
+
+- Solution URL: [Social media dashboard with theme switcher Solution](https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H/hub/htnl5-css3-flex-grid-sass-javascript-LAgWqOXBF)
+- Live Site URL: [Live Site](https://social-media-dashboard-site.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Sass (Syntactically awesome stylesheets)
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### Useful resources
+
+-[CSS Grid Detail](https://css-tricks.com/snippets/css/complete-guide-grid/).
+Please checkout the link to understand `Grid` better in full depth, very nicely elucidated by
+Chris House.
---
+++ js/script.js
@@ -0,0 +1,109 @@
+const hours = document.querySelectorAll("#hours");
+const lastWeek = document.querySelectorAll("#last-week");
+const dailyButton = document.querySelector(".dailyButton");
+const weeklyButton = document.querySelector(".weeklyButton");
+const monthlyButton = document.querySelector(".monthlyButton");
+const profileCardContainer = document.querySelector(".profile-card-container");
+const statsCard = document.querySelectorAll(".stats-card");
+const profileCard = document.querySelector(".profile-card");
+const weeks = document.querySelectorAll(".weeks ul li");
+
+const fetchStatistics = async () => {
+  const { data } = await axios.get("./data.json");
+  return data;
+};
+
+const onLoad = () => {
+  weeklyStatistics();
+  weeklyButton.classList.add("active");
+};
+
+const getDailyStatistics = (data) => {
+  for (let index = 0; index < hours.length; index++) {
+    data.map((dataStats, i) => {
+      hours[i].textContent = dataStats.timeframes.daily.current;
+      lastWeek[i].textContent = dataStats.timeframes.daily.previous;
+    });
+  }
+};
+
+const getWeeklyStatistics = (data) => {
+  for (let index = 0; index < hours.length; index++) {
+    data.map((dataStats, i) => {
+      hours[i].textContent = dataStats.timeframes.weekly.current;
+      lastWeek[i].textContent = dataStats.timeframes.weekly.previous;
+    });
+  }
+};
+
+const getMonthlyStatistics = (data) => {
+  for (let index = 0; index < hours.length; index++) {
+    data.map((dataStats, i) => {
+      hours[i].textContent = dataStats.timeframes.monthly.current;
+      lastWeek[i].textContent = dataStats.timeframes.monthly.previous;
+    });
+  }
+};
+
+const dailyStatistics = async () => {
+  const data = await fetchStatistics();
+  if (
+    weeklyButton.classList.contains("active") ||
+    monthlyButton.classList.contains("active")
+  ) {
+    weeklyButton.classList.remove("active");
+    monthlyButton.classList.remove("active");
+    dailyButton.classList.add("active");
+  }
+  getDailyStatistics(data);
+};
+
+const weeklyStatistics = async () => {
+  const data = await fetchStatistics();
+  if (
+    dailyButton.classList.contains("active") ||
+    monthlyButton.classList.contains("active")
+  ) {
+    dailyButton.classList.remove("active");
+    monthlyButton.classList.remove("active");
+    weeklyButton.classList.add("active");
+  }
+  getWeeklyStatistics(data);
+};
+
+const monthlyStatistics = async () => {
+  const data = await fetchStatistics();
+  if (
+    dailyButton.classList.contains("active") ||
+    weeklyButton.classList.contains("active")
+  ) {
+    dailyButton.classList.remove("active");
+    weeklyButton.classList.remove("active");
+    monthlyButton.classList.add("active");
+  }
+  getMonthlyStatistics(data);
+};
+
+// Event Listeners
+dailyButton.addEventListener("click", () => dailyStatistics());
+weeklyButton.addEventListener("click", () => weeklyStatistics());
+monthlyButton.addEventListener("click", () => monthlyStatistics());
+
+onLoad();
+
+// Animation
+const timeline = gsap.timeline();
+
+timeline
+  .from(profileCardContainer, {
+    autoAlpha: 0,
+    y: -100,
+    delay: 0.2,
+  })
+  .from(
+    profileCard,
+    { autoAlpha: 0, y: -100, stagger: 0.1, delay: 0.1 },
+    "-=0.2"
+  )
+  .from(weeks, { autoAlpha: 0, y: -100, stagger: 0.2, delay: 0.1 }, "-=0.2")
+  .from(statsCard, { autoAlpha: 0, y: -100, stagger: 0.1 }, "-=0.2");
